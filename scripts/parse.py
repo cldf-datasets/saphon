626004127ad701917f0f50241aca12e44ad88b48
lingulist <mattis.list@lingpy.org>
2021-05-28 00:45:08 +0200

    update

--- scripts/parse.py
+++ scripts/parse.py
@@ -1,6 +1,9 @@
 import pycldf
+import json
 
 saphon = pycldf.Dataset.from_metadata('../cldf/StructureDataset-metadata.json')
+
+D = {}
 
 with open("data.tsv", "w") as f:
     f.write("\t".join([
@@ -17,22 +20,43 @@ with open("data.tsv", "w") as f:
         "AllConsonants",
         "AllVowels",
         "AllDiphthongs"])+"\n")
+    for sound in saphon.objects("ParameterTable"):
+        try:
+            D["bipa-"+sound.data["CLTS_BIPA"]] = sound.data["CLTS_Name"]
+        except:
+            pass
     for language in saphon.objects("LanguageTable"):
         print("language {0}".format(language.name))
         sounds = [(
-            val.parameter.data["CLTS_BIPA"],
-            val.parameter.data["CLTS_Name"]
-            ) for val in language.values if val.parameter.data["CLTS_BIPA"]]
+            val.parameter.data["Name"],
+            val.parameter.data["CLTS_BIPA"] or "",
+            val.parameter.data["CLTS_Name"] or ""
+            ) for val in language.values]
         if sounds:
-            consonants = [sound for sound, name in sounds if
-                    name.endswith("consonant")]
-            vowels = [sound for sound, name in sounds if name.endswith("vowel")]
-            diphthongs = [sound for sound, name in sounds if
-                    name.endswith('diphthong')]
-            nasals = [sound for sound, name in sounds if "nasal" in name and
-                    (name.endswith("vowel") or name.endswith("diphthong"))]
-            longs = [sound for sound, name in sounds if "long" in name and
-                    (name.endswith("vowel") or name.endswith("diphthong"))]        
+            if language.data["Glottocode"]:
+                if language.data["Glottocode"] not in D:
+                    D[language.data["Glottocode"]] = []
+                D[language.data["Glottocode"]] += [{
+                        "ID": language.id,
+                        "Dataset": "saphon",
+                        "Name": language.name,
+                        "Source": "",
+                        "CLTS": {n: k or '?' for n, k, x in sounds},
+                        "Sounds": [n for n, k, x in sounds]
+                        }]
+
+            consonants = [sound for n, sound, name in sounds if
+                (name and name.endswith("consonant"))]
+            vowels = [sound for n, sound, name in sounds if (
+                name and name.endswith("vowel"))]
+            diphthongs = [sound for n, sound, name in sounds if
+                    (name and name.endswith('diphthong'))]
+            nasals = [sound for n, sound, name in sounds if "nasal" in name and
+                    ((name and name.endswith("vowel")) or (name and
+                        name.endswith("diphthong")))]
+            longs = [sound for n, sound, name in sounds if "long" in name and
+                    ((name and name.endswith("vowel")) or (name and
+                        name.endswith("diphthong")))]        
 
             f.write("\t".join(
                 [
@@ -53,3 +77,5 @@ with open("data.tsv", "w") as f:
         else:
             print('[!] no values for language {0}'.format(language.name))
 
+with open("data.js", "w") as f:
+    f.write("var DATA = "+json.dumps(D, indent=2)+";")
